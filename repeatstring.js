56b86c8c5392a46a993f753d60891c1470ecbb8c
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-04-21 17:51:05 +0200

    repeatstring done

---
+++ repeatstring.js
@@ -0,0 +1,20 @@
+// Write a function called repeatString which repeats the given String src exactly count times.
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+function repeatStr(count,string) {
+    let repeatedstring = ""
+    while(count>0) {
+      repeatedstring += string
+      count--
+    }
+    return repeatedstring
+  }
+
+
+////////////////////////////////////////////////////////
+
+
+function repeatStr(count,string) {
+    return string.repeat(count)
+}
