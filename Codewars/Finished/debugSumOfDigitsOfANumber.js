936e87be41b00591e8877f6bff1a7b7deb8f1c88
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-04-28 23:10:16 +0200

    done

--- Codewars/Finished/debugSumOfDigitsOfANumber.js
+++ Codewars/Finished/debugSumOfDigitsOfANumber.js
@@ -16,3 +16,9 @@ function getSumOfDigits(integer) {
     }
     return sum
 }
+
+// or shorter:
+
+function getSumOfDigits(integer) {
+    return (integer+'').split('').reduce((sum, d) => sum + (+d || 0), 0);
+  }
