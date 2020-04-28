829e312bbf21a39a2076bfe8b4a2e7fb008918b4
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-04-28 23:08:36 +0200

    done

---
+++ Codewars/Finished/debugSumOfDigitsOfANumber.js
@@ -0,0 +1,18 @@
+// Debug   function getSumOfDigits that takes positive integer to calculate sum of it's digits. 
+// Assume that argument is an integer.
+
+// Example
+// 123  => 6
+// 223  => 7
+// 1337 => 15
+
+
+function getSumOfDigits(integer) {
+    let arrint = integer.toString().split("")
+    console.log(arrint)
+    let sum = 0
+    for(var i=0;i<arrint.length;i++){
+      sum += Number(arrint[i])
+    }
+    return sum
+}
