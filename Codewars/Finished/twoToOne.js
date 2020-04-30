9acdab1f04e5414b9a30d558379634488d36dff1
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-04-30 11:25:21 +0200

    done

---
+++ Codewars/Finished/twoToOne.js
@@ -0,0 +1,26 @@
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+
+// each taken only once - coming from s1 or s2.
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1,s2) {
+	var result = [];
+	var temp = s1.concat(s2).split("").sort();
+	temp.reduce(function(previous,current){
+		if (current !== previous) {
+			result.push(current);
+			return current;
+		} else {
+			return previous;
+		}
+	},0);
+	return result.join("");
+}
+
+longest("bcdefghijklmnopqrstuvwxyz","abcdefghijklmnopqrstuvwxyz")
