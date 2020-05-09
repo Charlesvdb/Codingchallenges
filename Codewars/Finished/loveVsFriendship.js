80483120a7fa9806805b4674f8fc379e384eaa85
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-05-09 20:41:34 +0200

    done

--- Codewars/Finished/loveVsFriendship.js
+++ Codewars/Finished/loveVsFriendship.js
@@ -1,11 +1,7 @@
 // If　a = 1, b = 2, c = 3 ... z = 26
-
 // Then l + o + v + e = 54
-
 // and f + r + i + e + n + d + s + h + i + p = 108
-
 // So friendship is twice stronger than love :-)
-
 // The input will always be in lowercase and never be empty.
 
 
@@ -18,3 +14,8 @@ function wordsToMarks(string){
       }
     return sum
 }
+
+
+// or shorter:
+
+const wordsToMarks = s => [...s].reduce((res, c) => res += c.charCodeAt() - 96, 0)
