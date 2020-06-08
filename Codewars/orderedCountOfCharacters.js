dfe0dfdf153e97860555f9edc661db3341ae3f33
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-06-08 21:44:14 +0200

    done

---
+++ Codewars/orderedCountOfCharacters.js
@@ -0,0 +1,8 @@
+// Count the number of occurrences of each character and return it as a list of tuples in order of appearance. For empty output return an empty list.
+
+// Example:
+// orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
+
+const orderedCount = text => [...new Set(text)].map(letter => [letter, text.split(letter).length - 1])
+
+orderedCount("babbabbabbab")
